4bfffa15fc5303380a6d87789bf6d233138612e7
Jo Carter <jocarter+github@holler.co.uk>
2012-03-08 12:17:07 +0000

    Proper loading of dimension config for prod env with handler.

--- config/sfCmsPluginConfiguration.class.php
+++ config/sfCmsPluginConfiguration.class.php
@@ -27,6 +27,14 @@ class sfCmsPluginConfiguration extends sfPluginConfiguration
         'routing.load_configuration', 
         array('siteManager', 'routingLoadConfigurationListener')
       );
+      
+    // Register config handler for dimension app.yml files
+    if ($this->configuration instanceof sfApplicationConfiguration)
+    {
+      $configCache = $this->configuration->getConfigCache();
+      $configCache->registerConfigHandler('config/*/app.yml', 'sfDefineEnvironmentConfigHandler',
+        array('prefix' => 'app_'));
+    }
   }
   
   /**
@@ -34,38 +42,42 @@ class sfCmsPluginConfiguration extends sfPluginConfiguration
    */
   public function initialize()
   {
-    $config = ProjectConfiguration::getActive();
-  
+    // Don't do this if it's the command line
     if ('cli' != php_sapi_name())
     {
+      // We are dealing with a multi-site app.
       if (sfConfig::get('sf_app') == siteManager::getInstance()->getManagedApp() 
                   && class_exists('ysfApplicationConfiguration') 
-                  && $config instanceof ysfApplicationConfiguration)
+                  && $this->configuration instanceof ysfApplicationConfiguration)
       {
-        // We are dealing with a multi-site app.
-        // trying to replicate default factories behaviour as sfWebRequest not accessible here
-        $pathVar = sfConfig::get('app_site_path_info_array', 'SERVER');
-         
-        if ('SERVER' == $pathVar) $pathInfoArray = $_SERVER;
-        else $pathInfoArray = $_ENV;
-        
-        if (isset($pathInfoArray['HTTP_X_FORWARDED_HOST']))
+        // If dimension not set set and reload config
+        if (is_null($this->configuration->getDimension())) 
         {
-          $elements = explode(',', $pathInfoArray['HTTP_X_FORWARDED_HOST']);
-          $path = trim($elements[count($elements) - 1]);
-        }
-        else
-        {
-          $path =  isset($pathInfoArray['HTTP_HOST']) ? $pathInfoArray['HTTP_HOST'] : '';
-        }
-        
-        $dimension = sfConfig::get('app_dimensions_'.$path, siteManager::getInstance()->getDefaultSite());
-        
-        // Don't get stuck in a loop - if dimension not set set and reload config
-        if (is_null($config->getDimension())) 
-        {
-          $config->setDimension(array('site' => $dimension));
-          $config->initConfiguration(); // re-init to load the dimension's config/app.yml
+          // trying to replicate default factories behaviour as sfWebRequest not accessible here
+          $pathVar = sfConfig::get('app_site_path_info_array', 'SERVER');
+           
+          if ('SERVER' == $pathVar) $pathInfoArray = $_SERVER;
+          else $pathInfoArray = $_ENV;
+          
+          if (isset($pathInfoArray['HTTP_X_FORWARDED_HOST']))
+          {
+            $elements = explode(',', $pathInfoArray['HTTP_X_FORWARDED_HOST']);
+            $path = trim($elements[count($elements) - 1]);
+          }
+          else
+          {
+            $path =  isset($pathInfoArray['HTTP_HOST']) ? $pathInfoArray['HTTP_HOST'] : '';
+          }
+          
+          $dimension = sfConfig::get('app_dimensions_'.$path, siteManager::getInstance()->getDefaultSite());
+          
+          $this->configuration->setDimension(array('site' => $dimension));
+          
+          // Load dimensions config (if it exists) - requires the config handler registered above
+          if ($file = $this->configuration->getConfigCache()->checkConfig(sprintf('config/%s/app.yml', $dimension), true))
+          {
+            include($file);
+          }
         }
       }
     }
